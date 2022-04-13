92066e56090fcca793c4784f37e58cdea39939cf
Shridhar-Achari <shridharachar2706@gmail.com>
2022-04-13 15:17:09 +0530

    Add project files.

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World!\n");
+            Console.WriteLine("Hello, Welcome to console application\n");
+            var namme = Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome To Bootcamp\n Please Enter your name:");
+var name=Console.ReadLine();
